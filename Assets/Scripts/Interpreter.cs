66108583e192c352cf3e2e0849bfa0642df06603
ShaneStrander <shanestrander@gmail.com>
2024-01-27 14:36:21 -0600

    terminal header

--- Assets/Scripts/Interpreter.cs
+++ Assets/Scripts/Interpreter.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class Interpreter : MonoBehaviour
 {
     List<string> response = new List<string>();
+
+    List<string> errorResponses = new List<string>();
+
+    void Start()
+    {
+        errorResponses.Add("What are you trying to say?");
+        errorResponses.Add("That wasn't an option");
+        errorResponses.Add("You're pretty bad at following instructions");
+        errorResponses.Add("b r u h... type the right thing");
+        errorResponses.Add("Please listen to me next time");
+    }
+
     public List<string> Interpret(string userInput)
     {
         response.Clear();
 
         string[] args = userInput.Split();
 
+        string prevRes = args[0];
+
         if (args[0] == "Tasks")
         {
+            response.Add("Choose a task by typing it in the terminal to begin...");
             response.Add("Task1");
             response.Add("Task2");
             response.Add("Task3");
@@ -28,7 +43,9 @@ public class Interpreter : MonoBehaviour
         }
         else
         {
-            response.Add("idk what you typed");
+            int randomIndex = Random.Range(0, errorResponses.Count);
+            response.Add(errorResponses[randomIndex]);
+            response.Add("Type in the name of a Task below...");
 
             return response;
         }
