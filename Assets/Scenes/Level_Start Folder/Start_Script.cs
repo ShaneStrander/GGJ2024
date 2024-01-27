a2d2d2e870c666c29e19f6b164c892feb415b426
TheRealDemmy <Dendimir101@yahoo.com>
2024-01-27 16:07:15 +0100

    current push

--- Assets/Scenes/Level_Start Folder/Start_Script.cs
+++ Assets/Scenes/Level_Start Folder/Start_Script.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
-using System.Windows;
 using UnityEditor;
+<<<<<<< Updated upstream
 //using System.Windows.Forms;
+=======
+using UnityEngine.SceneManagement;
+>>>>>>> Stashed changes
 
 public class Start_Script : MonoBehaviour
 {
 
     [DllImport("user32.dll")]
-    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    public static extern int MessageBox2(IntPtr hWnd, string text, string caption, uint type);
 
     [DllImport("user32.dll")]
     private static extern IntPtr GetActiveWindow();
@@ -31,6 +34,30 @@ public class Start_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        bool result = EditorUtility.DisplayDialog("Confirmation", "Do you want to proceed?", "Yes", "No");
+
+        // Check the user's choice
+        if (result)
+        {
+            bool confirmedResult = EditorUtility.DisplayDialog("kek", "Excellent.", "OK");
+            if (confirmedResult)
+            {
+                SceneManager.LoadScene("TransparentWindow");
+            }    
+        }
+        else
+        {
+           bool secondResult = EditorUtility.DisplayDialog("w-what", "Pretty Please? Do it for the GameJam!", "Fine", "Nah");
+            if (secondResult)
+            {
+                SceneManager.LoadScene("TransparentWindow");
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
 
         IntPtr hWnd = GetActiveWindow();
         MARGINS margins = new MARGINS { cxLeftWidth = -1 };
