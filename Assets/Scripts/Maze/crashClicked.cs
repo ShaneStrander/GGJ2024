721fd9fae48dfa9273c81ebdeb9d88c4542ee5e8
ShaneStrander <shanestrander@gmail.com>
2024-01-27 17:54:00 -0600

    maze crash message

--- Assets/Scripts/Maze/crashClicked.cs
+++ Assets/Scripts/Maze/crashClicked.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class crashClicked : MonoBehaviour
 {
+
+    public GameObject message;
     private void OnMouseDown()
     {
         // Perform actions when the user clicks on the object
         Debug.Log("Object clicked: " + gameObject.name);
+        gameObject.SetActive(false);
+        message.SetActive(true);
     }
+
+
 }
